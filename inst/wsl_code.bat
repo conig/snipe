c2c3eef3f408c5fd76118f80e5376bb33c38c1a4
James Conigrave <james.conigrave@gmail.com>
2024-05-28 09:59:58 +1000

    handle non administrator execution

--- inst/wsl_code.bat
+++ inst/wsl_code.bat
@@ -1,6 +1,26 @@
 @echo off
 setlocal
 
+:: Change directory to the directory of the script
+cd /d "%~dp0"
+
+:: Check if the getadmin.vbs file exists in the temp directory and delete it if it does
+if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs"
+
+:: Query the dirty bit on the system drive, redirecting output to null
+:: If fsutil fails, the command will run as an elevated administrator
+fsutil dirty query %systemdrive% 1>nul 2>nul || (
+    
+    :: Create a VBScript to elevate the current script
+    echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && ""%~s0"" %params% && exit", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    
+    :: Execute the VBScript to request administrator privileges
+    "%temp%\getadmin.vbs"
+    
+    :: Exit the batch script to avoid continuing without elevation
+    exit /B
+)
+
 :: Get the current directory
 set "currentDir=%cd%"
 echo Current Directory: %currentDir%
