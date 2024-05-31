f4013ced4a5156dc147100294a67d989c15778fe
James Conigrave <james.conigrave@gmail.com>
2024-05-31 13:01:59 +1000

    update tar_bignetwork defaults, allow args to tar_visnetwork

--- R/tar_bignetwork.R
+++ R/tar_bignetwork.R
@@ -3,9 +3,10 @@
 #' Makes a bigger html widget tar_visnetwork
 #' @param width string including "px"
 #' @param height string including "px"
+#' @param ... additional arguments to targets::tar_visnetwork
 #' @export 
 
-tar_bignetwork <- function(width = "1020px", height = "980px") {
-  targets::tar_visnetwork() |>
+tar_bignetwork <- function(width = "1000px", height = "950px", ...) {
+  targets::tar_visnetwork(...) |>
     visNetwork::visOptions(width = width, height = height)
 }
